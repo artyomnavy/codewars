46482b91c1f194f00213188a7d234a12e9652ff2
artyomnavy <arteme.parygin@yandex.ru>
2023-08-03 21:54:46 +0300

    Commit sign comment

--- Even or Odd.js
+++ Even or Odd.js
@@ -1,5 +1,5 @@
-//Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
-//Solution:
+//❓DESCRIPTION: Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+//✔️SOLUTION:
 function evenOrOdd(number) {
     if (number % 2 == 0) {
       return 'Even'
