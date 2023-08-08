975b5391c588b4df8f7f42e0ba8ba2902d5d2ebc
artyomnavy <arteme.parygin@yandex.ru>
2023-08-08 21:23:14 +0300

    Opposite numbers

---
+++ Opposite number.js
@@ -0,0 +1,7 @@
+//❓DESCRIPTION: Very simple, given an integer or a floating-point number, find its opposite.
+//✔️SOLUTION:
+function opposite(number) {
+    let oppositeNum = -number;
+    return number, oppositeNum;
+}
+console.log(opposite(23));
