dd8c462446ab7ecec7bfd44367f033aa7fb5a10b
artyomnavy <arteme.parygin@yandex.ru>
2023-08-09 21:43:14 +0300

    Find the smallest integer in the array

---
+++ Find the smallest integer in the array.js
@@ -0,0 +1,8 @@
+//❓DESCRIPTION: Given an array of integers your solution should find the smallest integer.
+//✔️SOLUTION:
+class SmallestIntegerFinder {
+  findSmallestInt(args) {
+    return Math.min(...args);
+  }
+}
+console.log()
