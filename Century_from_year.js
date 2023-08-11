92a1a28b379584955962c0f353f2a05b71b07b95
artyomnavy <arteme.parygin@yandex.ru>
2023-08-11 21:07:55 +0300

    Century from year

---
+++ Century_from_year.js
@@ -0,0 +1,13 @@
+//❓DESCRIPTION:
+//The first century spans from the year 1 up to and including the year 100, the second century - from the year 101 up to and including the year 200, etc.
+//Given a year, return the century it is in.
+//✔️SOLUTION:
+function century(year) {
+    const cent = year / 100;
+    if (year % 100 === 0) {
+      return year / 100;
+    } else {
+      return (Math.floor(year / 100)) + 1;
+    }
+  }
+  console.log(century());
