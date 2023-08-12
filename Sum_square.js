881ec5abf1424cfe85fcea4970cb3ae48258ecf1
artyomnavy <arteme.parygin@yandex.ru>
2023-08-12 19:18:09 +0300

    Sum square improved solution

--- Sum_square.js
+++ Sum_square.js
@@ -2,15 +2,10 @@
 //Complete the square sum function so that it squares each number passed into it and then sums the results together.
 //✔️SOLUTION:
 function squareSum(numbers){
-  let array = [];
+  let sum = 0;
   
   for (let i = 0; i < numbers.length; i++) {
-    array.push(Math.pow(numbers[i], 2));
-  }
-  
-  let sum = 0;
-  for (let i = 0; i < array.length; i++ ) {
-    sum += array[i];
+    sum += Math.pow(numbers[i], 2);
   }
   
   return sum;
