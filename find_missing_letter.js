cea0f365d797c2de444d3b4d88bb3975a89c3be1
Artyom Parygin <arteme.parygin@yandex.ru>
2025-02-06 21:15:06 +0300

    added find_missing_letter

---
+++ find_missing_letter.js
@@ -0,0 +1,24 @@
+//❓DESCRIPTION:
+/*Find the missing letter
+
+Write a method that takes an array of consecutive (increasing) letters as input and that returns the missing letter in the array.
+
+You will always get an valid array. And it will be always exactly one letter be missing. The length of the array will always be at least 2.
+The array will always contain letters in only one case.
+
+Example:
+
+['a','b','c','d','f'] -> 'e'
+['O','Q','R','S'] -> 'P'
+
+(Use the English alphabet with 26 letters!)*/
+//✔️SOLUTION:
+function findMissingLetter(array) {
+  for (let i = 1; i < array.length; i++) {
+    if (array[i].charCodeAt() !== array[i - 1].charCodeAt() + 1) {
+      return String.fromCharCode(array[i].charCodeAt() - 1);
+    }
+  }
+}
+
+console.log(findMissingLetter(['a', 'b', 'c', 'd', 'f']));
